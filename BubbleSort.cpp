7eec097cbee450b84a85d8ea2f0637169faff8b1
Leonard <leopak2000@gmail.com>
2022-02-12 05:11:47 +0300

    Created Bubble Sort

---
+++ BubbleSort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+void BubbleSort(int arr[], int n);
+
+int main()
+{
+    const size_t n = 10;
+    int a[n];
+    
+    for (auto&& i : a)
+    {
+        i = rand() % 100;
+    }
+    for (const auto& i : a)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    BubbleSort(a, n);
+    
+    for (const auto& i : a)
+    {
+        cout << i << " ";
+    }
+    
+    return 0;
+}
+
+void BubbleSort(int arr[], int n)
+{
+    auto tmpN = 0;
+    for (size_t i = n; i > 0; --i)
+    {
+        for (size_t j = 0; j < i - 1; ++j)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                auto tmp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = tmp;
+            }
+            ++tmpN;
+        }
+    }
+    cout << n << " -> " << tmpN << endl;
+}
